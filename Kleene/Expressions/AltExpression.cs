5e5da58f27c69f3881125fe07ba3f5e296987d54
Josh Hufford <josh@hufford.io>
2020-06-09 20:10:51 -0400

    Implement AltExpression

---
+++ Kleene/Expressions/AltExpression.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kleene
+{
+    public class AltExpression<TIn, TOut> : VariadicExpression<TIn, TOut>
+    {
+        public AltExpression(IEnumerable<Expression<TIn, TOut>> expressions)
+            : base(expressions) { }
+
+        public override IEnumerable<IEnumerable<TOut>> Run(IEnumerable<TIn> input)
+        {
+            foreach (var expression in this.Expressions)
+            {
+                foreach (var result in expression.Run(input))
+                {
+                    yield return result;
+                }
+            }
+        }
+    }
+}
