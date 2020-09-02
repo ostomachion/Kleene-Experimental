0831cfeffd3ef597b882ad8553c6396b2dfb908d
Josh Hufford <josh@hufford.io>
2020-09-02 14:42:21 -0400

    Add more StructureExpression tests

--- KleeneTests/Expressions/StructureExpressionTests.cs
+++ KleeneTests/Expressions/StructureExpressionTests.cs
@@ -8,6 +8,30 @@ namespace KleeneTests
 {
     public class StructureExpressionTests
     {
+        public void NullName_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new StructureExpression(null!);
+            });
+        }
+
+        public void NullNameWithExpression_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new StructureExpression(null!, SequenceExpression.Empty);
+            });
+        }
+
+        public void NullExpression_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new StructureExpression("foo", null!);
+            });
+        }
+
         public void NoChildren_ReturnsSameNameNoChildren()
         {
             // Given
@@ -23,12 +47,57 @@ namespace KleeneTests
                     Assert.NotNull(result);
                     Assert.Equal("foo", result!.Name);
                     Assert.Collection(result.FirstChild,
-                        item => {
+                        item =>
+                        {
                             Assert.Null(item);
                         }
                     );
                     Assert.Collection(result.NextSibling,
-                        item => {
+                        item =>
+                        {
+                            Assert.Null(item);
+                        }
+                    );
+                }
+            );
+        }
+
+        public void WithExpression_ReturnsStructureWithChild()
+        {
+            // Given
+            var expression = new StructureExpression("foo", new StructureExpression("bar"));
+
+            // When
+            var results = expression.Run();
+
+            // Then
+            Assert.Collection(results,
+                result =>
+                {
+                    Assert.NotNull(result);
+                    Assert.Equal("foo", result!.Name);
+                    Assert.Collection(result.FirstChild,
+                        item =>
+                        {
+                            Assert.NotNull(item);
+                            Assert.Equal("bar", item!.Name);
+                            Assert.Collection(item.FirstChild,
+                                item =>
+                                {
+                                    Assert.Null(item);
+                                }
+                            );
+                            Assert.Collection(item.NextSibling,
+                                item =>
+                                {
+                                    Assert.Null(item);
+                                }
+                            );
+                        }
+                    );
+                    Assert.Collection(result.NextSibling,
+                        item =>
+                        {
                             Assert.Null(item);
                         }
                     );
