64cb7f98cfa23ba7e33b4102df00fe505026b86c
Josh Hufford <josh@hufford.io>
2020-09-03 19:52:43 -0400

    Add method to run an Expression against an input

--- Kleene/Expression.cs
+++ Kleene/Expression.cs
@@ -8,5 +8,10 @@ namespace Kleene
     public abstract class Expression
     {
         public abstract IEnumerable<NondeterministicStructure?> Run();
+
+        public IEnumerable<NondeterministicStructure?> Run(Structure input)
+        {
+            return new ConjunctionExpression((StructureExpression)input, this).Run();
+        }
     }
 }
