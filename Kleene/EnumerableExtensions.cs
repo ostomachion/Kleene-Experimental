47f1d3c6830e62fadc7f7d47e467efb91fdbf8e7
Josh Hufford <josh@hufford.io>
2021-03-12 09:53:39 -0500

    Rename class

--- Kleene/EnumerableExtensions.cs
+++ Kleene/EnumerableExtensions.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace Kleene
 {
-    public static class EnumerableExt
+    public static class EnumerableExtensions
     {
         public static IEnumerable<T> Yield<T>(T item) { yield return item; }
     }
