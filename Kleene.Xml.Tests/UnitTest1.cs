881eeeecbab74fc6ab46ff80468ff00d8d696daf
Josh Hufford <josh@hufford.io>
2021-03-12 12:41:07 -0500

    Add new XML projects

---
+++ Kleene.Xml.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Kleene.Xml.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Kleene.Xml/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Kleene.Xml
+{
+    public class Class1
+    {
+    }
+}
