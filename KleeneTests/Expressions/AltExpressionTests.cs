7b1b332deec12e6c9d95aed74481093e2c45895a
Josh Hufford <josh@hufford.io>
2021-03-12 09:33:13 -0500

    Rewrite more tests

--- KleeneTests/Expressions/AltExpressionTests.cs
+++ KleeneTests/Expressions/AltExpressionTests.cs
@@ -106,231 +106,232 @@ namespace KleeneTests
             }
         }
 
-        // public class RunOverload
-        // {
-        //     [Theory]
-        //     [InlineData('f', 'o')]
-        //     [InlineData(' ', '\t')]
-        //     public void FirstChoiceOfTwoChars_ReturnsFirstChar(char c1, char c2)
-        //     {
-        //         // Given
-        //         var item1 = (Expression)TextHelper.CreateStructure(c1);
-        //         var item2 = (Expression)TextHelper.CreateStructure(c2);
-        //         var expression = new AltExpression(new[] { item1, item2 });
-        //         var input = TextHelper.CreateStructure(c1);
+        public class RunOverload
+        {
+            [Theory]
+            [InlineData('f', 'o')]
+            [InlineData(' ', '\t')]
+            public void FirstChoiceOfTwoChars_ReturnsFirstChar(char c1, char c2)
+            {
+                // Given
+                var item1 = new RunnableExpression<char>(c1);
+                var item2 = new RunnableExpression<char>(c2);
+                var expression = new AltExpression<Runnable<char>>(new[] { item1, item2 });
+                var input = new Runnable<char>(c1);
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+                // When
+                var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Collection(results,
-        //             result =>
-        //             {
-        //                 Assert.Equal(input, result);
-        //             }
-        //         );
-        //     }
+                // Then
+                Assert.Collection(results,
+                    result =>
+                    {
+                        Assert.Equal(input, result);
+                    }
+                );
+            }
 
-        //     [Theory]
-        //     [InlineData('b', 'a', 'r')]
-        //     [InlineData(' ', '\r', '\n')]
-        //     public void ThreeChoicesEmptyInput_ReturnsEmpty(char c1, char c2, char c3)
-        //     {
-        //         // Given
-        //         var expression = new AltExpression(new[] {
-        //             (Expression)TextHelper.CreateStructure(c1),
-        //             (Expression)TextHelper.CreateStructure(c2),
-        //             (Expression)TextHelper.CreateStructure(c3),
-        //         });
-        //         var input = TextHelper.CreateStructure("");
+            [Theory]
+            [InlineData('b', 'a', 'r')]
+            [InlineData(' ', '\r', '\n')]
+            public void ThreeChoicesEmptyInput_ReturnsEmpty(char c1, char c2, char c3)
+            {
+                // Given
+                var expression = new AltExpression<ObjectSequence<Runnable<char>>>(new[] {
+                    c1.ToString().ToRunnable().ToExpression(),
+                    c2.ToString().ToRunnable().ToExpression(),
+                    c3.ToString().ToRunnable().ToExpression(),
+                });
+                var input = "".ToRunnable();
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+                // When
+                var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Empty(results);
-        //     }
+                // Then
+                Assert.Empty(results);
+            }
 
-        //     [Theory]
-        //     [InlineData('x', 'y', 'z')]
-        //     [InlineData('J', ' ', 'H')]
-        //     public void TwoChoicesNoMatch_ReturnsEmpty(char c1, char c2, char c)
-        //     {
-        //         // Given
-        //         Assert.DoesNotContain(c, new[] { c1, c2 });
-        //         var expression = new AltExpression(new[] {
-        //             (Expression)TextHelper.CreateStructure(c1),
-        //             (Expression)TextHelper.CreateStructure(c2)
-        //         });
-        //         var input = TextHelper.CreateStructure(c);
+            [Theory]
+            [InlineData('x', 'y', 'z')]
+            [InlineData('J', ' ', 'H')]
+            public void TwoChoicesNoMatch_ReturnsEmpty(char c1, char c2, char c)
+            {
+                // Given
+                Assert.DoesNotContain(c, new[] { c1, c2 });
+                var expression = new AltExpression<ObjectSequence<Runnable<char>>>(new[] {
+                    c1.ToString().ToRunnable().ToExpression(),
+                    c2.ToString().ToRunnable().ToExpression()
+                });
+                var input = c.ToString().ToRunnable();
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+                // When
+                var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Empty(results);
-        //     }
+                // Then
+                Assert.Empty(results);
+            }
 
-        //     [Theory]
-        //     [InlineData('b', 'a', 'r', ' ')]
-        //     [InlineData('u', 'v', 'w', 'U')]
-        //     [InlineData('J', 'o', 's', 'h')]
-        //     public void ThreeChoicesNoMatch_ReturnsEmpty(char c1, char c2, char c3, char c)
-        //     {
-        //         // Given
-        //         Assert.DoesNotContain(c, new[] { c1, c2, c3 });
-        //         var expression = new AltExpression(new[] {
-        //             (Expression)TextHelper.CreateStructure(c1),
-        //             (Expression)TextHelper.CreateStructure(c2),
-        //             (Expression)TextHelper.CreateStructure(c3)
-        //         });
-        //         var input = TextHelper.CreateStructure(c);
+            [Theory]
+            [InlineData('b', 'a', 'r', ' ')]
+            [InlineData('u', 'v', 'w', 'U')]
+            [InlineData('J', 'o', 's', 'h')]
+            public void ThreeChoicesNoMatch_ReturnsEmpty(char c1, char c2, char c3, char c)
+            {
+                // Given
+                Assert.DoesNotContain(c, new[] { c1, c2, c3 });
+                var expression = new AltExpression<ObjectSequence<Runnable<char>>>(new[] {
+                    c1.ToString().ToRunnable().ToExpression(),
+                    c2.ToString().ToRunnable().ToExpression(),
+                    c3.ToString().ToRunnable().ToExpression()
+                });
+                var input = c.ToString().ToRunnable();
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+                // When
+                var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Empty(results);
-        //     }
+                // Then
+                Assert.Empty(results);
+            }
 
-        //     [Theory]
-        //     [InlineData('x')]
-        //     [InlineData(' ')]
-        //     [InlineData('?')]
-        //     public void OneChoiceMatch_ReturnsChoice(char c)
-        //     {
-        //         // Given
-        //         var item = (Expression)TextHelper.CreateStructure(c);
-        //         var expression = new AltExpression(new[] { item });
-        //         var input = TextHelper.CreateStructure(c);
+            [Theory]
+            [InlineData('x')]
+            [InlineData(' ')]
+            [InlineData('?')]
+            public void OneChoiceMatch_ReturnsChoice(char c)
+            {
+                // Given
+                var expression = new AltExpression<ObjectSequence<Runnable<char>>>(new[] {
+                    c.ToString().ToRunnable().ToExpression()
+                });
+                var input = c.ToString().ToRunnable();
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+                // When
+                var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Collection(results,
-        //             result =>
-        //             {
-        //                 Assert.Equal(input, result);
-        //             }
-        //         );
-        //     }
+                // Then
+                Assert.Collection(results,
+                    result =>
+                    {
+                        Assert.Equal(input, result);
+                    }
+                );
+            }
 
-        //     [Theory]
-        //     [InlineData('x', 'y')]
-        //     [InlineData(' ', 'y')]
-        //     [InlineData('?', 'y')]
-        //     public void OneChoiceNoMatch_ReturnsEmpty(char c1, char c)
-        //     {
-        //         // Given
-        //         Assert.NotEqual(c1, c);
-        //         var expression = new AltExpression(new[] {
-        //             (Expression)TextHelper.CreateStructure(c1)
-        //         });
-        //         var input = TextHelper.CreateStructure(c);
+            // [Theory]
+            // [InlineData('x', 'y')]
+            // [InlineData(' ', 'y')]
+            // [InlineData('?', 'y')]
+            // public void OneChoiceNoMatch_ReturnsEmpty(char c1, char c)
+            // {
+            //     // Given
+            //     Assert.NotEqual(c1, c);
+            //     var expression = new AltExpression(new[] {
+            //         (Expression)TextHelper.CreateStructure(c1)
+            //     });
+            //     var input = TextHelper.CreateStructure(c);
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+            //     // When
+            //     var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Empty(results);
-        //     }
+            //     // Then
+            //     Assert.Empty(results);
+            // }
 
-        //     [Theory]
-        //     [InlineData('x')]
-        //     [InlineData('\\')]
-        //     [InlineData('\0')]
-        //     public void OneChoiceEmptyInput_ReturnsEmpty(char c)
-        //     {
-        //         // Given
-        //         var expression = new AltExpression(new[] {
-        //             (Expression)TextHelper.CreateStructure(c)
-        //         });
-        //         var input = TextHelper.CreateStructure("");
+            // [Theory]
+            // [InlineData('x')]
+            // [InlineData('\\')]
+            // [InlineData('\0')]
+            // public void OneChoiceEmptyInput_ReturnsEmpty(char c)
+            // {
+            //     // Given
+            //     var expression = new AltExpression(new[] {
+            //         (Expression)TextHelper.CreateStructure(c)
+            //     });
+            //     var input = TextHelper.CreateStructure("");
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+            //     // When
+            //     var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Empty(results);
-        //     }
+            //     // Then
+            //     Assert.Empty(results);
+            // }
 
-        //     [Theory]
-        //     [InlineData('x')]
-        //     [InlineData('\\')]
-        //     [InlineData('\0')]
-        //     public void ZeroChoices_ReturnsEmpty(char c)
-        //     {
-        //         // Given
-        //         var expression = new AltExpression(Enumerable.Empty<Expression>());
-        //         var input = TextHelper.CreateStructure(c);
+            // [Theory]
+            // [InlineData('x')]
+            // [InlineData('\\')]
+            // [InlineData('\0')]
+            // public void ZeroChoices_ReturnsEmpty(char c)
+            // {
+            //     // Given
+            //     var expression = new AltExpression(Enumerable.Empty<Expression>());
+            //     var input = TextHelper.CreateStructure(c);
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+            //     // When
+            //     var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Empty(results);
-        //     }
+            //     // Then
+            //     Assert.Empty(results);
+            // }
 
-        //     [Fact]
-        //     public void ZeroChoicesEmptyInput_ReturnsEmpty()
-        //     {
-        //         // Given
-        //         var expression = new AltExpression(Enumerable.Empty<Expression>());
-        //         var input = TextHelper.CreateStructure("");
+            // [Fact]
+            // public void ZeroChoicesEmptyInput_ReturnsEmpty()
+            // {
+            //     // Given
+            //     var expression = new AltExpression(Enumerable.Empty<Expression>());
+            //     var input = TextHelper.CreateStructure("");
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+            //     // When
+            //     var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Empty(results);
-        //     }
+            //     // Then
+            //     Assert.Empty(results);
+            // }
 
-        //     [Theory]
-        //     [InlineData('M')]
-        //     [InlineData('_')]
-        //     public void DuplicateChoice_ReturnChoiceTwice(char c)
-        //     {
-        //         // Given
-        //         var item1 = (Expression)TextHelper.CreateStructure(c);
-        //         var item2 = (Expression)TextHelper.CreateStructure(c);
-        //         var expression = new AltExpression(new[] { item1, item2 });
-        //         var input = TextHelper.CreateStructure(c);
+            // [Theory]
+            // [InlineData('M')]
+            // [InlineData('_')]
+            // public void DuplicateChoice_ReturnChoiceTwice(char c)
+            // {
+            //     // Given
+            //     var item1 = (Expression)TextHelper.CreateStructure(c);
+            //     var item2 = (Expression)TextHelper.CreateStructure(c);
+            //     var expression = new AltExpression(new[] { item1, item2 });
+            //     var input = TextHelper.CreateStructure(c);
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+            //     // When
+            //     var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Collection(results,
-        //             result =>
-        //             {
-        //                 Assert.Equal(input, result);
-        //             },
-        //             result =>
-        //             {
-        //                 Assert.Equal(input, result);
-        //             }
-        //         );
-        //     }
+            //     // Then
+            //     Assert.Collection(results,
+            //         result =>
+            //         {
+            //             Assert.Equal(input, result);
+            //         },
+            //         result =>
+            //         {
+            //             Assert.Equal(input, result);
+            //         }
+            //     );
+            // }
 
-        //     [Theory]
-        //     [InlineData('N')]
-        //     [InlineData('-')]
-        //     public void DuplicateChoiceEmptyInput_ReturnsEmpty(char c)
-        //     {
-        //         // Given
-        //         var expression = new AltExpression(new[] {
-        //             (Expression)TextHelper.CreateStructure(c),
-        //             (Expression)TextHelper.CreateStructure(c)
-        //         });
-        //         var input = TextHelper.CreateStructure("");
+            // [Theory]
+            // [InlineData('N')]
+            // [InlineData('-')]
+            // public void DuplicateChoiceEmptyInput_ReturnsEmpty(char c)
+            // {
+            //     // Given
+            //     var expression = new AltExpression(new[] {
+            //         (Expression)TextHelper.CreateStructure(c),
+            //         (Expression)TextHelper.CreateStructure(c)
+            //     });
+            //     var input = TextHelper.CreateStructure("");
 
-        //         // When
-        //         var results = expression.Run(input).SelectMany(x => x!.Collapse());
+            //     // When
+            //     var results = expression.Run(input).SelectMany(x => x!.Collapse());
 
-        //         // Then
-        //         Assert.Empty(results);
-        //     }
-        // }
+            //     // Then
+            //     Assert.Empty(results);
+            // }
+        }
     }
 }
